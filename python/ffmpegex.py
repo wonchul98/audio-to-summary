38bf49205ba69edb5b9eb8c68afad6b8c8aa9870
wonchul98 <shinwc1998@naver.com>
2023-03-06 13:49:19 +0900

    d

---
+++ python/ffmpegex.py
@@ -0,0 +1,3 @@
+from pydub import AudioSegment
+
+AudioSegment.ffmpeg = "C:\\Users\\Administrator\\Desktop\\졸업 프로젝트\\audio-to-summary\\python\\ffmpeg-master-latest-win64-gpl-shared\\bin\\ffmpeg.exe"
