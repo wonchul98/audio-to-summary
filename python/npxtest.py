4c30886f706e23a66efa2990dae7191ede2383cc
wonchul98 <89337820+wonchul98@users.noreply.github.com>
2023-03-25 20:38:28 +0900

    0325 commit

--- python/npxtest.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from scipy.io.wavfile import write
-
-rate = 44100
-data = np.random.uniform(-1, 1, rate) # 1 second worth of random samples between -1 and 1
-print(data.shape)
-scaled = np.int16(data / np.max(np.abs(data)) * 32767)
-#write('test.wav', rate, scaled)
