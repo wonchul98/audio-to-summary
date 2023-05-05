d689ec1e36b1d9016ccdb8ef60a94cc935f4ab44
wonchul98 <89337820+wonchul98@users.noreply.github.com>
2023-05-05 20:30:28 +0900

    data commit

--- python/cudaenable.py
+++
@@ -1,5 +0,0 @@
-import torch
-print(torch.__version__)
-print(torch.cuda.is_available())
-cuda = torch.device('cuda')
-print(torch.cuda.is_available())
--- python/cut_wav.py
+++ python/cut_wav.py
@@ -1,31 +1,33 @@
+import os
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 
+def save_segment(segment, index, folder):
+    segment.export(os.path.join(folder, f"segment_{index}.wav"), format="wav")
+
 # 오디오 파일을 불러옵니다.
-sound_file = AudioSegment.from_wav("hamburger.wav")
+sound_file = AudioSegment.from_wav("samgpt.wav")
 
 # 음성 부분만 분리합니다.
 chunks = split_on_silence(sound_file, min_silence_len=700, silence_thresh=-50)
 
-# 10초를 기준으로 자르기 위한 변수입니다.
-ten_seconds = 10 * 1000
+# segments 폴더를 생성합니다.
+if not os.path.exists("segments"):
+    os.makedirs("segments")
 
-result = []
-
-# 분리된 오디오 파일을 저장합니다.
-for i, chunk in enumerate(chunks):
-    # 0.5초보다 짧은 파일은 이전 파일과 합칩니다.
-    if len(chunk) < 500:
-        if i > 0:
-            last_chunk = result[-1]
-            result[-1] = last_chunk + chunk
-        else:
-            result.append(chunk)
-    # 10초를 기준으로 파일을 자릅니다.
+# 길이가 0.5초보다 작은 경우 이전 파일과 합치고, 20초가 넘으면 자르는 코드
+merged_chunks = []
+for chunk in chunks:
+    if len(chunk) < 500:  # 0.5초보다 작으면
+        if merged_chunks:
+            merged_chunks[-1] += chunk  # 이전 파일과 합치기
+    elif len(chunk) > 20000:  # 20초가 넘으면
+        while len(chunk) > 20000:
+            part, chunk = chunk[:20000], chunk[20000:]
+            merged_chunks.append(part)
     else:
-        chunk_count = len(chunk) // ten_seconds + 1
-        for j in range(chunk_count):
-            start = j * ten_seconds
-            end = (j + 1) * ten_seconds
-            sub_chunk = chunk[start:end]
-            sub_chunk.export(f"cut_wav/sep_ham_{i}_{j}.wav", format="wav")
+        merged_chunks.append(chunk)
+
+# segments 폴더에 segment_{i}.wav 형식으로 분리된 오디오를 저장합니다.
+for i, chunk in enumerate(merged_chunks):
+    save_segment(chunk, i, "segments")
